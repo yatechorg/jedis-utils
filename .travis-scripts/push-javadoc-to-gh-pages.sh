ff3318a52038438518540fd751840766e93706ab
Yinon Avraham <yinonavraham@gmail.com>
2015-09-19 10:27:27 +0300

    Use "snapshot" instead of "latest"

--- .travis-scripts/push-javadoc-to-gh-pages.sh
+++ .travis-scripts/push-javadoc-to-gh-pages.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_REPO_SLUG" == "yatechorg/jedis-utils" ] && [ "$TRAVIS_JDK_VERSION"
 
   echo -e "Publishing javadoc...\n"
 
-  cp -R build/docs/javadoc $HOME/javadoc-latest
+  cp -R build/docs/javadoc $HOME/javadoc-snapshot
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -13,9 +13,9 @@ if [ "$TRAVIS_REPO_SLUG" == "yatechorg/jedis-utils" ] && [ "$TRAVIS_JDK_VERSION"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/yatechorg/jedis-utils gh-pages > /dev/null
 
   cd gh-pages
-  git rm -rf ./javadoc/latest
-  mkdir -p ./javadoc/latest
-  cp -Rf $HOME/javadoc-latest/* ./javadoc/latest
+  git rm -rf ./javadoc/snapshot
+  mkdir -p ./javadoc/snapshot
+  cp -Rf $HOME/javadoc-snapshot/* ./javadoc/snapshot
   git add -f .
   git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
