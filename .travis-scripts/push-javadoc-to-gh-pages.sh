1e660d05da51d675f90cb7759a57c3c72d910106
yinona <yinona@jfrog.com>
2015-09-20 08:58:50 +0300

    Add support for different versions in pushing the javadocs, not just snapshot

--- .travis-scripts/push-javadoc-to-gh-pages.sh
+++ .travis-scripts/push-javadoc-to-gh-pages.sh
@@ -3,9 +3,15 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "yatechorg/jedis-utils" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
 
-  echo -e "Publishing javadoc...\n"
+  VERSION=`./gradlew -q getVersion`
+  if [[ $VERSION != *SNAPSHOT* ]]; then
+    JAVADOC_VER=$VERSION
+  else
+    JAVADOC_VER=snapshot
+  fi
+  echo -e "Publishing $JAVADOC_VER javadoc...\n"
 
-  cp -R build/docs/javadoc $HOME/javadoc-snapshot
+  cp -R build/docs/javadoc $HOME/javadoc-$JAVADOC_VER
 
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
@@ -13,13 +19,13 @@ if [ "$TRAVIS_REPO_SLUG" == "yatechorg/jedis-utils" ] && [ "$TRAVIS_JDK_VERSION"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/yatechorg/jedis-utils gh-pages > /dev/null
 
   cd gh-pages
-  git rm -rf ./javadoc/snapshot
-  mkdir -p ./javadoc/snapshot
-  cp -Rf $HOME/javadoc-snapshot/* ./javadoc/snapshot
+  git rm -rf ./javadoc/$JAVADOC_VER
+  mkdir -p ./javadoc/$JAVADOC_VER
+  cp -Rf $HOME/javadoc-$JAVADOC_VER/* ./javadoc/$JAVADOC_VER
   git add -f .
-  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages (version=$VERSION)"
   git push -fq origin gh-pages > /dev/null
 
-  echo -e "Published Javadoc to gh-pages.\n"
+  echo -e "Published $JAVADOC_VER javadoc to gh-pages.\n"
   
 fi
